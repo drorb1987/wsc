a89a522419f33afdce3f29a168ae8cf73bfb0247
drorb1987 <drorb1987@gmail.com>
2022-05-13 14:33:27 +0300

    chaning all file

--- wsc.py
+++ wsc.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 from torchvision.io import read_image
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
@@ -6,61 +7,29 @@ import pandas as pd
 import glob
 import os
 
-LABELS_TO_NUMS = {'Angle': 0,
-                  'Box': 1,
-                  'Circle': 2,
-                  'Closeup': 3,
-                  'Crowd': 4,
-                  'Other': 5}
+classes = ['Angle', 'Box', 'Circle', 'Closeup', 'Crowd', 'Other']
 
-NUMS_TO_LABELS = {val: key for key, val in LABELS_TO_NUMS.items()}
+transform = transforms.Compose([transforms.Resize([720, 1280]), transforms.ToTensor()])
+training_dataset = torchvision.datasets.ImageFolder(root='./train/', target_transform=transform)
+training_loader = DataLoader(training_dataset, batch_size=len(training_dataset))
 
-class WscDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+# Calculate the mean and std
+dataiter = iter(training_loader)
+images, _ = dataiter.next()
+imgs = images.view(images.size(0), images.size(1), -1)
+mean = imgs.mean(2).sum(0) / imgs.size(0)
+std = imgs.std(2).sum(0) / imgs.size(0)
 
-    def __len__(self):
-        return len(self.img_labels)
+# Normalized dataset
+normalized_transform = transforms.Compose([
+    transforms.Resize([720, 1280]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+    ])
 
-    def __getitem__(self, index: int):
-        img_path = os.path.join(self.img_dir, self.img_labels.label.iloc[index], self.img_labels.image.iloc[index])
-        image = read_image(img_path)
-        label = self.img_labels.label.iloc[index]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            image = self.target_transform(image)
-        return image, label
+batch_size = 4
+normalized_training_dataset = torchvision.datasets.ImageFolder(root='./train/', target_transform=normalized_transform)
+normalized_testing_dataset = torchvision.datasets.ImageFolder(root='./test/', target_transform=normalized_transform)
+normalized_training_loader = DataLoader(normalized_training_dataset, batch_size=batch_size, shuffle=True)
+normalized_testing_loader = DataLoader(normalized_testing_dataset, batch_size=batch_size, shuffle=False)
 
-
-def make_csv():
-    """
-    Check if csv files of the data are existed, if not create them
-    """
-    for tt in ["train", "test"]:
-        annotations_file = f"{tt}\\labeled_data.csv"
-        if not os.path.exists(annotations_file):
-            imgs = glob.glob(f"{tt}\*\*")
-            labels = []
-            images = []
-            for img in imgs:
-                parse_img = img.split('\\')
-                labels.append(parse_img[1])
-                images.append(parse_img[2])
-            df = pd.DataFrame({"label": labels, "image": images})
-            df.to_csv(annotations_file)
-
-def main():
-    make_csv()
-    training_data = WscDataset('train/labeled_data.csv', 'train', transform=transforms.Resize([720, 1280]))
-    validation_data = WscDataset('test/labeled_data.csv', 'test', transform=transforms.Resize([720, 1280]))
-    training_loader = DataLoader(dataset=training_data, batch_size=4, shuffle=True, num_workers=0)
-    validation_loader = DataLoader(dataset=validation_data, batch_size=4, shuffle=False, num_workers=0)
-
-
-
-if __name__ == '__main__':
-    main()
